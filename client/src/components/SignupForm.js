36eeb8ffc8b9f55d18d25c1e9d91a9a108757312
Jason Lam <nyjasonlam@gmail.com>
2021-06-20 23:29:56 -0400

    refactored signupform to use apollo

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client'
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
+import { CREATE_USER } from '../utils/mutations'
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +14,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [createUser, { error }] = useMutation(CREATE_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +32,9 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await createUser({
+        variables: userFormData
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -55,7 +61,7 @@ const SignupForm = () => {
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         {/* show alert if server response is bad */}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your signup!
+          {error.message}
         </Alert>
 
         <Form.Group>
